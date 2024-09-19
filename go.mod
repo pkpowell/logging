cbf3a2898bdd87864b36bc91b8ff09dc91909235
phil <p@pkpowell.net>
2024-09-19 12:22:23 +0200

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module logging
+
+go 1.23.1
+
+require github.com/lmittmann/tint v1.0.5
---
+++ logging.go
@@ -0,0 +1,134 @@
+package logging
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"os"
+	"runtime"
+	"time"
+
+	"github.com/lmittmann/tint"
+)
+
+const (
+	YYYYMMDD            = "2006-01-02"
+	HHMMSS24h           = "15:04:05"
+	HHMMSS12h           = "3:04:05 PM"
+	TextDate            = "January 2, 2006"
+	TextDateWithWeekday = "Monday, January 2, 2006"
+	AbbrTextDate        = "Jan 2 Mon"
+)
+
+var (
+	logger, errLogger           *slog.Logger
+	handler, errHandler         *slog.HandlerOptions
+	tintHandler, errTintHandler *tint.Options
+	logLevel                    = &slog.LevelVar{} // INFO
+)
+
+func Init(verbose *bool, jsonLogs *bool) (DisplayLog, DisplayLog, DisplayLog, DisplayLog, CreateError) {
+	handler = &slog.HandlerOptions{
+		Level: logLevel,
+	}
+
+	errHandler = &slog.HandlerOptions{
+		Level:     logLevel,
+		AddSource: true,
+	}
+
+	tintHandler = &tint.Options{
+		Level: logLevel,
+	}
+
+	errTintHandler = &tint.Options{
+		Level:     logLevel,
+		AddSource: true,
+	}
+
+	switch true {
+	case *verbose:
+		fmt.Println("Verbose logging")
+		logLevel.Set(slog.LevelDebug)
+		handler.AddSource = true
+		tintHandler.AddSource = true
+
+	case *jsonLogs:
+		logger = slog.New(slog.NewJSONHandler(os.Stderr, handler))
+		errLogger = slog.New(slog.NewJSONHandler(os.Stderr, errHandler))
+
+	default:
+		tintHandler.TimeFormat = HHMMSS24h
+		errTintHandler.TimeFormat = HHMMSS24h
+
+		logger = slog.New(tint.NewHandler(os.Stderr, tintHandler))
+		errLogger = slog.New(tint.NewHandler(os.Stderr, errTintHandler))
+
+	}
+
+	// if *verbose {
+	// } else {
+	// }
+
+	// if *jsonLogs {
+	// } else {
+	// }
+
+	slog.SetDefault(logger)
+
+	return Infof, Errorf, Warnf, Debugf, Errorw
+}
+
+type DisplayLog func(string, ...any)
+type CreateError func(string, error) error
+
+func Infof(format string, args ...any) {
+	if !logger.Enabled(context.Background(), slog.LevelInfo) {
+		return
+	}
+	var pcs [1]uintptr
+	runtime.Callers(2, pcs[:]) // skip [Callers, Infof]
+	r := slog.NewRecord(time.Now(), slog.LevelInfo, fmt.Sprintf(format, args...), pcs[0])
+	_ = logger.Handler().Handle(context.Background(), r)
+}
+
+func Warnf(format string, args ...any) {
+	if !logger.Enabled(context.Background(), slog.LevelWarn) {
+		return
+	}
+	var pcs [1]uintptr
+	runtime.Callers(2, pcs[:]) // skip [Callers, Infof]
+	r := slog.NewRecord(time.Now(), slog.LevelWarn, fmt.Sprintf(format, args...), pcs[0])
+	_ = logger.Handler().Handle(context.Background(), r)
+}
+
+func Debugf(format string, args ...any) {
+	if !logger.Enabled(context.Background(), slog.LevelDebug) {
+		return
+	}
+	var pcs [1]uintptr
+	runtime.Callers(2, pcs[:]) // skip [Callers, Infof]
+	// fmt.Println("pcs: ", pcs[0])
+	r := slog.NewRecord(time.Now(), slog.LevelDebug, fmt.Sprintf(format, args...), pcs[0])
+	_ = logger.Handler().Handle(context.Background(), r)
+}
+
+func Errorf(format string, args ...any) {
+	if !errLogger.Enabled(context.Background(), slog.LevelError) {
+		return
+	}
+	var pcs [1]uintptr
+	runtime.Callers(2, pcs[:]) // skip [Callers, Infof]
+	r := slog.NewRecord(time.Now(), slog.LevelError, fmt.Sprintf(format, args...), pcs[0])
+	_ = errLogger.Handler().Handle(context.Background(), r)
+}
+
+func Errorw(format string, err error) error {
+	if !errLogger.Enabled(context.Background(), slog.LevelError) {
+		return nil
+	}
+	var pcs [1]uintptr
+	runtime.Callers(2, pcs[:]) // skip [Callers, Infof]
+	r := slog.NewRecord(time.Now(), slog.LevelError, fmt.Errorf(format+" %w", err.Error()).Error(), pcs[0])
+	return errLogger.Handler().Handle(context.Background(), r)
+}

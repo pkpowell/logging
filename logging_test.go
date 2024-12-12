7787d39d3b47887c7b7a65f6b3f0256898323bf2
phil <p@pkpowell.net>
2024-12-12 10:00:29 +0100

    updates

--- logging_test.go
+++ logging_test.go
@@ -1,1 +1,37 @@
 package logging
+
+import (
+	"io"
+	"log"
+	"os"
+	"testing"
+)
+
+// SETUP
+// you need to call Run() once you've done what you need
+func TestMain(m *testing.M) {
+	log.SetOutput(io.Discard)
+	os.Exit(m.Run())
+}
+
+func BenchmarkInfo(b *testing.B) {
+	b.ReportAllocs()
+	Init(bp(false), bp(false))
+
+	for range 50 {
+		Info("Hello", "World")
+	}
+}
+
+func BenchmarkInfof(b *testing.B) {
+	b.ReportAllocs()
+	Init(bp(false), bp(false))
+
+	for range 50 {
+		Infof("Hello %s", "World")
+	}
+}
+
+func bp(b bool) *bool {
+	return &b
+}

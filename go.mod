e4736f7cfc1dbe666dcc1a4da04b5edbd1a7819a
phil <p@pkpowell.net>
2025-01-25 13:15:05 +1300

    add http logging

--- go.mod
+++ go.mod
@@ -5,6 +5,11 @@ go 1.23.2
 require github.com/lmittmann/tint v1.0.6
 
 require (
+	github.com/govalues/decimal v0.1.35 // indirect
+	github.com/pkpowell/humanize v0.5.3 // indirect
+)
+
+require (
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/stretchr/testify v1.10.0 // indirect
---
+++ http.go
@@ -0,0 +1,57 @@
+package logging
+
+import (
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/pkpowell/humanize/units"
+)
+
+type wrappedWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+var (
+	start    time.Time
+	duration time.Duration
+	length   int
+	ww       *wrappedWriter
+	cl       string
+	d        string
+)
+
+func HTTPLogs(h http.Handler, verbose *bool, webLogs *bool) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var err error
+		start = time.Now()
+		length = 0
+
+		ww = &wrappedWriter{
+			ResponseWriter: w,
+			statusCode:     http.StatusOK,
+		}
+
+		h.ServeHTTP(ww, r)
+
+		cl = w.Header().Get("Content-Length")
+		if cl != "" {
+			length, err = strconv.Atoi(cl)
+			if err != nil {
+				Error("strconv.Atoi error", err.Error())
+				// length = -1
+			}
+		}
+
+		duration = time.Since(start)
+
+		d = r.Proto + " from " + r.RemoteAddr + " " + strconv.Itoa(ww.statusCode) + " " + r.Method + " " + r.RequestURI + " " + duration.String() + " " + units.Int(length).String()
+
+		if *webLogs && !*verbose {
+			Info(d)
+		} else {
+			Debug(d)
+		}
+	})
+}
